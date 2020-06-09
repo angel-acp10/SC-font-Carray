c8e484bd4b3e9bb08328f8b5aad84bc019112cc6
Angel Castro <angel.castro.palomares@gmail.com>
2020-06-09 20:55:54 +0200

    Input file addition & creation of DataInput class.

--- Carray-font-gen.py
+++ Carray-font-gen.py
@@ -1,18 +1,96 @@
 import freetype
 import math
+import unicodedata
+
 face = freetype.Face("helvetica.ttf")
 size = 10
 
 face.set_pixel_sizes( 0, size )
 
 class Metadata:
-    def __init__(self, char, width, height, y_offset):
-        self.char = char
+    def __init__(self, unicode):
+        self.unicode = unicode
+
+    def setParameters(width, height, y_offset):
         self.width = width
         self.height = height
         #each char is placed from the top left corner, y_offset sets the vertical offset from that corner
         self.y_offset = y_offset 
 
+class DataInput:
+    def __init__(self):
+        self.range_start, self.range_end = [], []
+        self.string = ''
+
+        f = open("input.txt", "r")
+        line = f.readline()
+        while line:
+            line = line.strip()
+            line = line.replace(' ','')
+            line = line.replace('\t', '')
+
+            if line[0]=='[' and line[-1]==']' and line.find(',')!=-1:
+                line = line.strip('[')
+                line = line.strip(']')
+                rg = line.split(',',1)
+                rg = [int(i,0) for i in rg]
+                self.range_start.append(rg[0])
+                self.range_end.append(rg[1])
+
+            elif line[0]=='"' and line[-1]=='"':
+                self.string += line.strip('"')
+            
+            line = f.readline()
+        f.close()
+
+        print(self.range_start)
+        print(self.range_end)
+        print(self.string)
+
+        self.Sort()
+        self.FindRanges()
+
+    def Sort(self):
+        self.buff_list = []
+        
+        #save all the unicode indexes within the introduced range in a buffer list
+        for start, end in zip(self.range_start, self.range_end):
+            for uc_index in range(start,end+1):
+                self.buff_list.append(uc_index)
+        
+        for char in self.string:
+            self.buff_list.append(ord(char))
+
+        #removes duplicates
+        self.buff_list = list(set(self.buff_list)) 
+        print(self.buff_list)
+
+        self.buff_list.sort() #sorts the list
+        print(self.buff_list)
+
+    def FindRanges(self):
+        fr_start = [self.buff_list[0]]
+        fr_end = [self.buff_list[0]]
+        for i in range(len(self.buff_list)-1):
+            if self.buff_list[i]+1 == self.buff_list[i+1]:
+                fr_end[-1] += 1
+            else:
+                fr_start.append(self.buff_list[i+1])
+                fr_end.append(self.buff_list[i+1])
+        
+        print(fr_start)
+        print(fr_end)
+
+
+
+
+
+
+        
+
+
+
+'''
 class Font(object):
     def __init__(self, filename, size):
         self.face = freetype.Face(filename)
@@ -64,7 +142,7 @@ class Font(object):
 
             f_height = self.font_height
             # full slot
-            
+
             while y_offset:
                 for i in range(width):
                     print( format( 0, '#010b')+', ', end = '' )
@@ -88,9 +166,16 @@ class Font(object):
             
         for char_info in metadata:
             print(str(char_info.char) + '--> w='+str(char_info.width) + '\th='+str(char_info.height) + '\tyoff='+str(char_info. y_offset) )
-
+'''
             
 
-font = Font('helvetica.ttf', 8)
-font.write_to_C_file('abcdefghijklmnopqrstuvwxyz')
-        
+#font = Font('helvetica.ttf', 8)
+#font.write_to_C_file('abcdefghijklmnopqrstuvwxyz')
+'''
+c='ε'
+c=ord(c)
+print(c)
+print(type(c))
+'''
+
+data = DataInput()
